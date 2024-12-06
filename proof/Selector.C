6e7def284d21a9e89ea0262cea9b69af9b3569b2
Alfredo Castaneda <castaned@cern.ch>
2024-12-06 16:47:55 +0100

    update Selectr

--- proof/Selector.C
+++ proof/Selector.C
@@ -196,9 +196,9 @@ Int_t Selector(std::string outputLabel, std::string files = "", Int_t fWorkers =
          or line.find("/store") == std::string::npos) continue;
       if( nfend == -1 or
           (counter >= nfstart and counter <= nfend)){
-          line = Form("root://eoscms.cern.ch/%s",line.c_str());
-        //line = Form("root://cmsxrootd.fnal.gov/%s",line.c_str());
-        //line = Form("root://cms-xrd-global.cern.ch/%s",line.c_str());
+	//          line = Form("root://eoscms.cern.ch/%s",line.c_str());
+	  //line = Form("root://cmsxrootd.fnal.gov/%s",line.c_str());
+	  line = Form("root://cms-xrd-global.cern.ch/%s",line.c_str());
         std::cout << "Chaining [" << counter << ":" << fChain->GetNtrees() << "]\t" << line << "\t";
         int ans = fChain->AddFile(line.c_str());
         if(ans == 1){
