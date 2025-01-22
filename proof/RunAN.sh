79fbce0d5c2843978537bb39fcea7d9c3efbb18b
Alfredo <castaned@cern.ch>
2025-01-22 17:05:40 -0600

    updated

--- proof/RunAN.sh
+++ proof/RunAN.sh
@@ -5,27 +5,28 @@ YEARP=$2 #2016->2018
 TYPEP=$3 #MC or DATA
 SAMPLEFILENAME=$4 # ULX.txt
 OutputLabel=$5
-NFSTART=${6:0} #FromFile
-NFEND=${7:-1} #ToFile
+NFSTART=${6:-0} #FromFile
+NFEND=${7:--1} #ToFile
 SPACING_FACTOR=${8:-1.15}
 LIMIT_EDGE=${9:-1350.0}
 FIRSTBIN_LEFTEDGE=${10:-60.0}
 FIRSTBIN_RIGHTEDGE=${11:-70.0}
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc7_amd64_gcc900
-scram project CMSSW CMSSW_12_2_1
-cd CMSSW_12_2_1/src
+export SCRAM_ARCH=el9_amd64_gcc12
+scram project CMSSW CMSSW_13_3_0_pre4
+cd CMSSW_13_3_0_pre4/src
+cmsenv
 eval `scram runtime -sh`
 echo  "CMSSW Dir: "$CMSSW_BASE
-git clone --branch $BRANCHNAME https://github.com/vargasa/WprimeSearch.git Wprime_$YEARP
+git clone --branch $BRANCHNAME https://github.com/castaned/WprimeSearch Wprime_$YEARP
 WprimeDir=$PWD/Wprime_$YEARP/
 echo "Analysis Dir: "$WprimeDir
 #sed -i 's/cmsxrootd.fnal.gov/xrootd-cms.infn.it/' $WprimeDir/proof/Selector.C
 #sed -i 's/cmsxrootd.fnal.gov/xrootd.unl.edu/' $WprimeDir/proof/Selector.C
-#sed -i 's/cmsxrootd.fnal.gov/cms-xrd-global.cern.ch/' $WprimeDir/proof/Selector.C
+sed -i 's/cmsxrootd.fnal.gov/cms-xrd-global.cern.ch/' $WprimeDir/proof/Selector.C
 cd $WprimeDir/proof
-wget -c https://avargash.web.cern.ch/avargash/WprimeSearch/x509up_u114404
-export X509_USER_PROXY=$PWD/x509up_u114404
+wget -c https://raw.githubusercontent.com/castaned/files/refs/heads/main/x509up_u47450
+export X509_USER_PROXY=$PWD/x509up_u47450
 voms-proxy-info -all
 voms-proxy-info -all -file $X509_USER_PROXY
 
@@ -50,5 +51,5 @@ root -l -b -q "Selector.C("$ROOTCommand")";
 cd $WprimeDir/proof/
 for i in `ls WprimeHistos_*.root`;
 do
-    xrdcp -vf $i root://cmseos.fnal.gov//store/user/avargash/WprimeSearchCondorOutput/$i
+#    xrdcp -vf $i root://cmseos.fnal.gov//store/user/avargash/WprimeSearchCondorOutput/$i
 done
